4423e48d364460f73db76b19b56ba412fcf10b8c
Ishaan Sharma <ishaan328069@gmail.com>
2022-08-15 10:12:06 -0400

    Add files via upload

---
+++ Views/SidebarContentView(donotopen).swift
@@ -0,0 +1,110 @@
+//
+//  SidebarContentView.swift
+//  News
+//
+//  Created by Ishaan Sharma on 7/19/22.
+//
+
+import SwiftUI
+
+struct SidebarContentView: View {
+    
+    @Binding var selectedMenuItemId: MenuItem.ID?
+    private var selection: Binding<MenuItem.ID?> {
+        Binding {
+            selectedMenuItemId ?? MenuItem.category(.general).id
+        } set: { newValue in
+            if let menuItemId = newValue {
+                selectedMenuItemId = menuItemId
+            }
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                navigationLink
+                
+                List(selection: selection) {
+                    ForEach([MenuItem.saved, MenuItem.search]) {
+                        listRow($0)
+                    }
+                    
+                    Section {
+                        ForEach(Category.menuItems) {
+                            listRow($0)
+                        }
+                    } header: {
+                        Text("Categories")
+                    }
+                    Divider()
+                    Text("Made by Ishaan Sharma")
+                        .bold()
+                        .foregroundStyle(.secondary)
+                        .padding(.bottom)
+                }
+                .navigationTitle("News")
+                .listStyle(.sidebar)
+            }
+        }
+    }
+    
+    private func navigationLinkForMenuItem(_ item: MenuItem) -> some View {
+        NavigationLink(destination: viewForMenuItem(item: item).environmentObject(ArticleBookmarkViewModel.shared), tag: item.id, selection: selection) {
+            Label(item.text, systemImage: item.systemImage)
+        }
+    }
+    
+    @ViewBuilder
+    private func listRow(_ item: MenuItem) -> some View {
+        let isSelected = item.id == selection.wrappedValue
+        Button {
+            self.selection.wrappedValue = item.id
+        } label: {
+            Label(item.text, systemImage: item.systemImage)
+        }
+        .foregroundColor(isSelected ? Color.white : nil)
+        .listRowBackground((isSelected ? Color.accentColor : Color.clear).mask(RoundedRectangle(cornerRadius: 8)))
+    }
+    
+    @ViewBuilder
+    private var navigationLink: some View {
+        if let selectedMenuItem = MenuItem(id: selection.wrappedValue) {
+            NavigationLink(
+                destination: viewForMenuItem(item: selectedMenuItem).environmentObject(ArticleBookmarkViewModel.shared),
+                tag: selectedMenuItem.id,
+                selection: selection) {
+                    EmptyView()
+                }
+        }
+    }
+    
+    
+    @ViewBuilder
+    private func viewForMenuItem( item: MenuItem) -> some View {
+        switch item {
+        case .search:
+            SearchTabView()
+        case .saved:
+            BookmarkTabView()
+        case .category(let category):
+            NewsTabView(category: category)
+                .id(category.id)
+        }
+    }
+}
+
+struct SidebarContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        SidebarContentView(selectedMenuItemId: .constant(nil))
+    }
+}
+
+
+
+
+
+
+
+
+
